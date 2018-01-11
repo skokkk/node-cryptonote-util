e234221c486607db27439e0230f8e286b8f545d9
skokkk <jean-pierre@specternetworks.com>
2018-01-11 16:23:45 +0200

    asdasd
    asdasd

--- src/crypto/crypto.cpp
+++ src/crypto/crypto.cpp
@@ -221,7 +221,9 @@ DISABLE_VS_WARNINGS(4200)
 POP_WARNINGS
 
   static inline size_t rs_comm_size(size_t pubs_count) {
-    return sizeof(rs_comm) + pubs_count * sizeof(rs_comm().ab[0]);
+    // note: https://github.com/zone117x/node-cryptonote-pool/issues/175 final post
+    // return sizeof(rs_comm) + pubs_count * sizeof(rs_comm().ab[0]);
+    return sizeof(rs_comm) + pubs_count * 2 * sizeof(ec_point); /* BEWARE: Dependent on rs_comm definition above! */
   }
 
   void crypto_ops::generate_ring_signature(const hash &prefix_hash, const key_image &image,
